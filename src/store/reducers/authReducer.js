3fd89b168c07be67cf02baf8a14e1ff54e91eed9
nenu4142 <nenu4142@gmail.com>
2021-06-24 11:39:32 +0530

    refactored authReducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -21,99 +21,183 @@ const initialState = {
   },
 };
 
+// helper functions
+
+const authStart = (state) => {
+  return { ...state, loading: true };
+};
+
+const authEnd = (state) => {
+  return { ...state, loading: false };
+};
+
+const authFail = (state, payload) => {
+  return { ...state, error: payload };
+};
+
+const authSuccess = (state) => {
+  return { ...state, error: false };
+};
+
+const verifyStart = (state) => {
+  return {
+    ...state,
+    verifyEmail: { ...state.verifyEmail, loading: true },
+  };
+};
+
+const verifySuccess = (state) => {
+  return {
+    ...state,
+    verifyEmail: { ...state.verifyEmail, loading: false, error: false },
+  };
+};
+
+const verifyFail = (state, payload) => {
+  return {
+    ...state,
+    verifyEmail: { ...state.verifyEmail, loading: false, error: payload },
+  };
+};
+
+const recoveryStart = (state) => {
+  return {
+    ...state,
+    recoverPassword: { ...state.recoverPassword, loading: true },
+  };
+};
+
+const recoverySuccess = (state) => {
+  return {
+    ...state,
+    recoverPassword: {
+      ...state.recoverPassword,
+      loading: false,
+      error: false,
+    },
+  };
+};
+
+const recoveryFail = (state, payload) => {
+  return {
+    ...state,
+    recoverPassword: {
+      ...state.recoverPassword,
+      loading: false,
+      error: payload,
+    },
+  };
+};
+
+const profileEditStart = (state) => {
+  return {
+    ...state,
+    profileEdit: { ...state.profileEdit, loading: true },
+  };
+};
+
+const profileEditFail = (state, payload) => {
+  return {
+    ...state,
+    profileEdit: { ...state.profileEdit, loading: false, error: payload },
+  };
+};
+
+const profileEditSuccess = (state) => {
+  return {
+    ...state,
+    profileEdit: { ...state.profileEdit, loading: false, error: false },
+  };
+};
+
+const deleteStart = (state) => {
+  return { ...state, deleteUser: { ...state.deleteUser, loading: true } };
+};
+
+const deleteFail = (state, payload) => {
+  return {
+    ...state,
+    deleteUser: { ...state.deleteUser, loading: false, error: payload },
+  };
+};
+
+const cleanUp = (state) => {
+  return {
+    ...state,
+    error: null,
+    loading: false,
+    verifyEmail: {
+      ...state.verifyEmail,
+      loading: false,
+      error: null,
+    },
+    recoverPassword: {
+      ...state.recoverPassword,
+      loading: false,
+      error: null,
+    },
+    profileEdit: {
+      ...state.profileEdit,
+      loading: false,
+      error: null,
+    },
+    deleteUser: {
+      ...state.deleteUser,
+      loading: false,
+      error: null,
+    },
+  };
+};
+
 const authReducer = (state = initialState, { type, payload }) => {
   switch (type) {
+    case actions.CLEAN_UP:
+      return cleanUp(state);
+
     case actions.AUTH_START:
-      return { ...state, loading: true };
+      return authStart(state);
 
     case actions.AUTH_END:
-      return { ...state, loading: false };
+      return authEnd(state);
 
     case actions.AUTH_FAIL:
-      return { ...state, error: payload };
+      return authFail(state, payload);
 
     case actions.AUTH_SUCCESS:
-      return { ...state, error: false };
-
-    case actions.CLEAN_UP:
-      return {
-        ...state,
-        error: null,
-        loading: false,
-        verifyEmail: { ...state.verifyEmail, loading: false, error: null },
-        recoverPassword: {
-          ...state.recoverPassword,
-          loading: false,
-          error: null,
-        },
-        profileEdit: { ...state.profileEdit, loading: false, error: null },
-        deleteUser: { ...state.deleteUser, loading: false, error: null },
-      };
+      return authSuccess(state);
 
     case actions.VERIFY_START:
-      return { ...state, verifyEmail: { ...state.verifyEmail, loading: true } };
+      return verifyStart(state);
 
     case actions.VERIFY_SUCCESS:
-      return {
-        ...state,
-        verifyEmail: { ...state.verifyEmail, loading: false, error: false },
-      };
+      return verifySuccess(state);
 
     case actions.VERIFY_FAIL:
-      return {
-        ...state,
-        verifyEmail: { ...state.verifyEmail, loading: false, error: payload },
-      };
+      return verifyFail(state, payload);
 
     case actions.RECOVERY_START:
-      return {
-        ...state,
-        recoverPassword: { ...state.recoverPassword, loading: true },
-      };
+      return recoveryStart(state);
 
     case actions.RECOVERY_SUCCESS:
-      return {
-        ...state,
-        recoverPassword: {
-          ...state.recoverPassword,
-          loading: false,
-          error: false,
-        },
-      };
+      return recoverySuccess(state);
 
     case actions.RECOVERY_FAIL:
-      return {
-        ...state,
-        recoverPassword: {
-          ...state.recoverPassword,
-          loading: false,
-          error: payload,
-        },
-      };
+      return recoveryFail(state, payload);
 
     case actions.PROFILE_EDIT_START:
-      return { ...state, profileEdit: { ...state.profileEdit, loading: true } };
+      return profileEditStart(state);
 
     case actions.PROFILE_EDIT_SUCCESS:
-      return {
-        ...state,
-        profileEdit: { ...state.profileEdit, loading: false, error: false },
-      };
+      return profileEditSuccess(state);
 
     case actions.PROFILE_EDIT_FAIL:
-      return {
-        ...state,
-        profileEdit: { ...state.profileEdit, loading: false, error: payload },
-      };
+      return profileEditFail(state, payload);
 
     case actions.DELETE_START:
-      return { ...state, deleteUser: { ...state.deleteUser, loading: true } };
+      return deleteStart(state);
 
     case actions.DELETE_FAIL:
-      return {
-        ...state,
-        deleteUser: { ...state.deleteUser, loading: false, error: payload },
-      };
+      return deleteFail(state, payload);
 
     default:
       return state;
